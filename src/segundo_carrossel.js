0e7145da70124291d2a83493fa6e427c04b6b68f
EncryptKalean <kauanle78@gmail.com>
2025-09-20 09:12:19 -0300

    Add files via upload

--- src/segundo_carrossel.js
+++ src/segundo_carrossel.js
@@ -1,54 +1,53 @@
-const blocos = document.querySelectorAll('.bloco');
+// Carrossel
+const itens = document.querySelectorAll('.bloco');
 const trilho = document.getElementById('trilho');
 const styles_trilho = window.getComputedStyle(trilho);
 const tamanho_do_trilho = +styles_trilho.getPropertyValue('width').split('px')[0];
 
-// Pega o tamanho dos blocos de acordo com o tamanho do trilho
-const tamanho_bloco = tamanho_do_trilho / blocos.length;
+// Pega o tamanho dos itens de acordo com o tamanho do trilho
+const tamanho_bloco = tamanho_do_trilho / itens.length;
 
-/* Quantas casas vão aparecer na vitrine */
-const quantidade_exposta = getComputedStyle(document.documentElement).getPropertyValue('--quantidade_exposta').trim();
+/* Número de itens visíveis simultaneamente no carrossel */
+const quantidade_exposta = +getComputedStyle(document.documentElement).getPropertyValue('--quantidade_exposta');
 
-/* Tempo que o carrossel demora para trocar a casa */
+/* Tempo que o carrossel demora para andar */
 const velocidade = getComputedStyle(document.documentElement).getPropertyValue('--tempo_carrossel');
 
-/* Quantas casas vão ser puladas por giro */
+/* Quantidade de itens que o carrossel avança por ciclo */
 const valorCss = +getComputedStyle(document.documentElement).getPropertyValue('--casas_por_giro');
 const casa_por_giro = valorCss > quantidade_exposta ? quantidade_exposta : valorCss;
 
-let progresso_carrossel = blocos.length;
+let casa_carrossel = quantidade_exposta;
 
 function carrossel() {
+
+    // Faz o calculo de quantos itens faltam
+    const falta = itens.length - casa_carrossel;
+
     // Pega o valor de translateX do trilho
     const matrix = new DOMMatrixReadOnly(styles_trilho.transform);
     let translateX = +matrix.m41;
 
-    // Atualiza o progresso do carrossel, subtraindo as casas percorridas
-    progresso_carrossel -= casa_por_giro;
-
     // Se não houver mais casas para percorrer, reinicia o carrossel
-    if (progresso_carrossel <= 0) {
-        progresso_carrossel = blocos.length;
+    if (casa_carrossel >= itens.length) {
         translateX = 0;
+        casa_carrossel = quantidade_exposta - casa_por_giro;
     }
 
-    // Se o número de casas restantes for menor que o valor de casas por giro,
-    // desacelera o carrossel proporcionalmente ao tamanho dos blocos
-    else if (progresso_carrossel <= quantidade_exposta) {
-        if (quantidade_exposta > casa_por_giro) progresso_carrossel = quantidade_exposta - casa_por_giro;
-        translateX -= tamanho_bloco * progresso_carrossel;
-    }
+    // Se o número de casas_por_giro for maior que a quantidade de casas faltantes,
+    // desacelera o carrossel proporcionalmente ao tamanho dos itens
+    else if (casa_por_giro > falta && falta > 0) translateX -= tamanho_bloco * falta;
 
     // Caso contrário, continua normalmente com o deslocamento padrão
     else translateX -= tamanho_bloco * casa_por_giro;
 
+    // Atualiza o progresso do carrossel, somando as casas percorridas
+    casa_carrossel += casa_por_giro;
+
     // Aplica a nova posição ao trilho
     trilho.style.setProperty('transform', `translateX(${translateX}px)`);
-
-    // console.log("progresso: " + progresso_carrossel);
-    // console.log("casa por giro: " + casa_por_giro);
-    // console.log("quantidade exposta: " + quantidade_exposta);
 };
 
-
-setInterval(() => { carrossel() }, velocidade * 1000);
+// Ativa o carrossel apenas se houver mais itens do que os visíveis simultaneamente
+let ativar_carrossel;
+if (itens.length > quantidade_exposta) ativar_carrossel = setInterval(() => { carrossel() }, velocidade * 1000);
